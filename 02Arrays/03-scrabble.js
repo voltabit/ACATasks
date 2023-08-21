7498a3d7c4f5c86fcc178dae359a75c3d313b879
Karo Kendimian <karo.kendimian@outlook.com>
2023-08-21 18:05:05 +0400

    Scrabbles

--- 02Arrays/03-scrabble.js
+++ 02Arrays/03-scrabble.js
@@ -1,1 +1,23 @@
 "use strict";
+const wordToCheck = "Rammstein";
+
+function calcScrabbleScore(word) {
+  const charScores = ["", "aeioulnrst", "dg", "bcmp", "fhvwy", "k", "jx", "qz"];
+  let score = 0;
+
+  word = word.toLowerCase();
+
+  for (let i = 0; i < word.length; i++) {
+    for (let j = 0; j < charScores.length; j++) {
+      for (let k = 0; k < charScores[j].length; k++) {
+        if (word.charAt(i) === charScores[j][k]) {
+          score += j;
+          break;
+        }
+      }
+    }
+  }
+  return score;
+}
+
+console.log(calcScrabbleScore(wordToCheck));
