2fdbe6211b5d1195b8d684da379463dd9aadc760
Karo Kendimian <chesmorana@gmail.com>
2023-08-23 12:27:23 +0400

    sort and filter - revised

--- 01Objects_Arrays/13 Sort&Filter.js
+++ 01Objects_Arrays/13 Sort&Filter.js
@@ -5,18 +5,24 @@ Output the books sorted by the percent in descending order which readStatus is t
 */
 
 const readProgress = [
-    { book: "Catcher in the Rye", readStatus: true, percent: 40 },
-    { book: "Animal Farm", readStatus: true, percent: 20 },
-    { book: "Solaris", readStatus: false, percent: 90 },
-    { book: "The Fall", readStatus: true, percent: 50 },
-    { book: "White Nights", readStatus: false, percent: 60 },
-    { book: "After Dark", readStatus: true, percent: 70 },
-   ];
+  { book: "Catcher in the Rye", readStatus: true, percent: 40 },
+  { book: "Animal Farm", readStatus: true, percent: 20 },
+  { book: "Solaris", readStatus: false, percent: 90 },
+  { book: "The Fall", readStatus: true, percent: 50 },
+  { book: "White Nights", readStatus: false, percent: 60 },
+  { book: "After Dark", readStatus: true, percent: 70 },
+];
 
-readProgress.sort((a, b) =>{
-    return a.percent - b.percent;
-});
+function sortFilterBooks(books) {
+  let myBooks = books
+    .filter((el) => el.readStatus === true)
+    .sort((a, b) => {
+      return a.percent - b.percent;
+    });
 
-readProgress.forEach((e) => {
+  myBooks.forEach((e) => {
     console.log(`${e.percent}% \t ${e.book}`);
-});
+  });
+}
+
+sortFilterBooks(readProgress);
