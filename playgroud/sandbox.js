6d602c81c3fd58c0e4577d9017f3f2939542c37b
Karo Kendimian <karo.kendimian@outlook.com>
2023-08-24 00:54:10 +0400

    ,

--- playgroud/sandbox.js
+++ playgroud/sandbox.js
@@ -1,3 +1,13 @@
-const b = function (a) {return a};
-
-console.log(b(99));
+switch (true) {
+    case isSquare(shape):
+      console.log("This shape is a square.");
+    // Fall-through, since a square is a rectangle as well!
+    case isRectangle(shape):
+      console.log("This shape is a rectangle.");
+    case isQuadrilateral(shape):
+      console.log("This shape is a quadrilateral.");
+      break;
+    case isCircle(shape):
+      console.log("This shape is a circle.");
+      break;
+  }
