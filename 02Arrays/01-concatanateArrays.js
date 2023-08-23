23fdb148f78d76543bb27e0861375114b97b21ad
Karo Kendimian <chesmorana@gmail.com>
2023-08-23 12:47:48 +0400

    add concat arrays

---
+++ 02Arrays/01-concatanateArrays.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const arr1 = [1, 2, 3, 4];
+const arr2 = [5, 6, 7, 8, 9];
+
+function myConcat(array1, array2) {
+  return array1.concat(array2);
+}
+
+console.log(myConcat(arr1, arr2));
