aee1385526e559e9034b0f4628aabc20306c5104
Karo Kendimian <chesmorana@gmail.com>
2023-08-24 11:57:08 +0400

    t

--- playgroud/sandbox.js
+++ playgroud/sandbox.js
@@ -1,3 +1,12 @@
-const b = function (a) {return a};
+const a = 1;
+function b() {
+    a = 10;
+    return;
+    function a() {}
+}
+b();
+console.log(a);
+var user = "t";
+var user = "s";
 
-console.log(b(99));
+console.log(user);
