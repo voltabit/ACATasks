01a79b80b45c1c7ece87b5a6bc78b40dc9cca751
Karo Kendimian <chesmorana@gmail.com>
2023-09-11 16:15:24 +0400

    6 improved

--- 04Recursions/06BinarySearch.js
+++ 04Recursions/06BinarySearch.js
@@ -1,11 +1,17 @@
 "use strict";
 
 function binarySearch(arr, num, start = 0, end = arr.length - 1) {
-  if (num < arr[0] || num > arr[arr.length - 1] || arr.length === 0) return -1;
+  if (
+    arr.length === 0 ||
+    num < arr[0] ||
+    num > arr[arr.length - 1] ||
+    (start === end && arr[start] !== num)
+  )
+    return -1;
 
   let middle = Math.round((start + end) / 2);
   if (num === arr[middle]) return middle;
-  if (start === end && arr[start] !== num) return -1;
+
   if (num < arr[middle]) return binarySearch(arr, num, start, middle - 1);
   else return binarySearch(arr, num, middle + 1, end);
 }
