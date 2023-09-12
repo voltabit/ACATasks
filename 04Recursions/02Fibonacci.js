b3ae16b93b08c9629353a1419c20d27a2ebe1e1e
Karo Kendimian <chesmorana@gmail.com>
2023-09-12 18:13:32 +0400

    simplify fibo

--- 04Recursions/02Fibonacci.js
+++ 04Recursions/02Fibonacci.js
@@ -3,8 +3,7 @@
 function fibonacci(number) {
   if (number === 0) return 0;
   if (number === 1) return 1;
-  if (number === 2) return 1;
   return fibonacci(number - 1) + fibonacci(number - 2);
 }
 
-console.log(fibonacci(5));
+console.log(fibonacci(10));
--- 04Recursions/12Permutations.js
+++ 04Recursions/12Permutations.js
@@ -1,4 +1,22 @@
 "use strict";
-function permutations(str, result = []) {}
+function permutations(string) {
+  const results = [];
+  if (string.length === 1) {
+    results.push(string);
+    return results;
+  }
 
-console.log(permutations("hello"));
+  for (let i = 0; i < string.length; i++) {
+    let firstChar = string[i];
+    let rest = string.substring(0, i) + string.substring(i + 1);
+    let subPermutations = permutations(rest);
+    for (let j = 0; j < subPermutations.length; j++) {
+      const permutation = firstChar + subPermutations[j];
+      if (results.includes(permutation)) continue;
+      results.push(permutation);
+    }
+  }
+  return results;
+}
+
+console.log(permutations("test"));
