feb5727218e3fb7b3aaf92cf291d5a793d3f9fb7
Karo Kendimian <chesmorana@gmail.com>
2023-09-13 17:00:08 +0400

    13

--- 04Recursions/13Combinations.js
+++ 04Recursions/13Combinations.js
@@ -1,7 +1,19 @@
-"use strict";
-function generateCombinations(arr) {
-  result = [];
-  for (let i = 0; i < arr.length; i++) {}
+function generateCombinations(array, size) {
+  const result = [];
+
+  const combinations = (arr, start, depth, currentCombination) => {
+    if (depth === 0) {
+      result.push([...currentCombination]);
+      return;
+    }
+    for (let i = start; i <= arr.length - depth; i++) {
+      currentCombination.push(arr[i]);
+      combinations(arr, i + 1, depth - 1, currentCombination);
+      currentCombination.pop();
+    }
+  };
+  combinations(array, 0, size, []);
+  return result;
 }
 
 const inputArray = [1, 2, 3, 4];
