538ba0620a72bfa200a543897ed466157620ea0c
Karo Kendimian <chesmorana@gmail.com>
2023-09-14 18:40:02 +0400

    14

--- 04Recursions/14FindSubset.js
+++ 04Recursions/14FindSubset.js
@@ -1,17 +1,22 @@
-// INCOMPLETE
+function isSubsetSum(array, checkNumber) {
+  let n = array.length;
 
-"use strict";
-function subsetSum(arr, sum) {
-  const subset = [...arr];
-  const firstNumber = subset.shift();
-
-  if (subset.length > 0) subsetSum(subset, sum - firstNumber);
-  for (let i = 0; i < subset.length; i++) {
-    const subSum = firstNumber + subset[i];
-    if (subSum === sum) return true;
+  function subsetSum(array, n, checkNumber) {
+    if (checkNumber === 0) {
+      return true;
+    }
+    if (n === 0 && checkNumber !== 0) {
+      return false;
+    }
+    if (array[n - 1] > checkNumber) {
+      return subsetSum(array, n - 1, checkNumber);
+    }
+    return (
+      subsetSum(array, n - 1, checkNumber) ||
+      subsetSum(array, n - 1, checkNumber - array[n - 1])
+    );
   }
-
-  return false;
+  return subsetSum(array, n, checkNumber);
 }
-const inputArray = [2, 4, 6, 8];
-console.log(subsetSum(inputArray, 10));
+let array = [2, 4, 6, 8];
+console.log(isSubsetSum(array, 20));
