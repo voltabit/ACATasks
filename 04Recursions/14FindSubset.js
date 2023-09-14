2ca0f75925d71026e7ceb6d07ab5c5dcba5b3b7d
Karo Kendimian <chesmorana@gmail.com>
2023-09-14 18:41:25 +0400

    14 simplify

--- 04Recursions/14FindSubset.js
+++ 04Recursions/14FindSubset.js
@@ -2,15 +2,9 @@ function isSubsetSum(array, checkNumber) {
   let n = array.length;
 
   function subsetSum(array, n, checkNumber) {
-    if (checkNumber === 0) {
-      return true;
-    }
-    if (n === 0 && checkNumber !== 0) {
-      return false;
-    }
-    if (array[n - 1] > checkNumber) {
-      return subsetSum(array, n - 1, checkNumber);
-    }
+    if (checkNumber === 0) return true;
+    if (n === 0 && checkNumber !== 0) return false;
+    if (array[n - 1] > checkNumber) return subsetSum(array, n - 1, checkNumber);
     return (
       subsetSum(array, n - 1, checkNumber) ||
       subsetSum(array, n - 1, checkNumber - array[n - 1])
