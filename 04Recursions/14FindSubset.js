eeebf26d29b88c2291e99a2dd2f1e3f341712d6a
Karo Kendimian <chesmorana@gmail.com>
2023-09-14 15:47:42 +0400

    removing redundant folder

---
+++ 04Recursions/14FindSubset.js
@@ -0,0 +1,14 @@
+"use strict";
+function subsetSum(arr, sum) {
+  const subset = [...arr];
+  const firstNumber = subset.shift();
+
+  if (subset.length > 0) subsetSum(subset, sum - firstNumber);
+
+  const subSum = firstNumber + su;
+  if (subSum === sum) return true;
+
+  return false;
+}
+const inputArray = [2, 4, 6, 8];
+console.log(subsetSum(inputArray, 10));
