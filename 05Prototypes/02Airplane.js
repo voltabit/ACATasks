31dbe32413590fa24758484d2bf2a4983b9df79d
Karo Kendimian <chesmorana@gmail.com>
2023-09-15 18:34:53 +0400

    airplane

--- 05Prototypes/02Airplane.js
+++ 05Prototypes/02Airplane.js
@@ -1,6 +1,23 @@
 /* Write an Airplane object that initializes name.
 Give airplanes the ability to .takeOff() and .land():
 If a plane takes off, its isFlying property is set to true.
-If a plane lands, its isFlying property is set to false.
+If a plane lands, its isFlying property is set to false.*/
 
-Write your own call, apply and bind methods */
+const Airplane = {
+  init(name) {
+    this.name = name;
+    this.isFlying = false;
+  },
+  takeOff() {
+    this.isFlying = true;
+    return console.log(`${this.name} is flying.`);
+  },
+  land() {
+    this.isFlying = false;
+    return console.log(`${this.name} has landed.`);
+  },
+};
+
+Airplane.init("Mig 21");
+Airplane.takeOff();
+Airplane.land();
