a554dddc41b0752145f06836975530202cff62e8
Karo Kendimian <chesmorana@gmail.com>
2023-09-15 16:42:18 +0400

    gitignore

---
+++ 05Prototypes/01Pagination.js
@@ -0,0 +1,61 @@
+const Pagination = {
+  page: 1,
+  init(arr, pageSize) {
+    let count = 0;
+    const book = [];
+    do {
+      const pageContents = [];
+      for (let i = count; i < count + pageSize; i++) {
+        if (arr[i] === undefined) continue;
+        pageContents.push(arr[i]);
+      }
+      book.push(pageContents);
+      count += pageSize;
+    } while (count < arr.length);
+    this.book = book;
+    return book;
+  },
+
+  getPageItems() {
+    return console.log(this.book[this.page - 1]);
+  },
+
+  nextPage() {
+    if (this.page < this.book.length) {
+      this.page = this.page + 1;
+    }
+    return this;
+  },
+  prevPage() {
+    if (this.page <= 2) {
+      this.page = 1;
+    } else {
+      this.page = this.page - 1;
+    }
+    return this;
+  },
+  firstPage() {
+    this.page = 1;
+    return this;
+  },
+  lastPage() {
+    this.page = this.book.length;
+    return this;
+  },
+  goToPage(targetPage) {
+    if (targetPage < 1 || targetPage > this.book.length) return this.page;
+    this.page = targetPage;
+    return this.page;
+  },
+};
+
+const alphabetArray = "abcdefghijklmnopqrstuvwxyz".split("");
+
+Pagination.init(alphabetArray, 4); // Do the pagination
+Pagination.getPageItems(); // ["a", "b", "c", "d"]
+Pagination.nextPage(); // add the current page by one
+Pagination.getPageItems(); // ["e", "f", "g", "h"]
+Pagination.nextPage().nextPage(); // the ability to call chainable
+Pagination.getPageItems();
+Pagination.goToPage(3); // current page must be set to 3
+Pagination.getPageItems();
---
+++ 05Prototypes/02Airplane.js
@@ -0,0 +1,6 @@
+/* Write an Airplane object that initializes name.
+Give airplanes the ability to .takeOff() and .land():
+If a plane takes off, its isFlying property is set to true.
+If a plane lands, its isFlying property is set to false.
+
+Write your own call, apply and bind methods */
