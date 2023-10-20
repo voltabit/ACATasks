f1503be604e5988e35c72c453d8284e589d316fb
Karo Kendimian <chesmorana@gmail.com>
2023-10-20 09:50:12 +0400

    Resubmit lesson 8 tasks

--- 08Classes/02Shiritori.js
+++ 08Classes/02Shiritori.js
@@ -4,30 +4,38 @@ class Shiritori {
   game_over = false;
 
   play = function (word) {
-    this.game_over = false;
-    if (this.words.length === 0) {
-      this.game_over = false;
-      this.words.push(word);
-    }
     if (this.game_over === true) {
-      return console.log("Game is Over!");
+      return "Game is Over!";
     }
-    if (
-      word[0] != this.words[this.words.length - 1].at(-1) &&
-      !(word in this.words)
+    if (this.words.length === 0) {
+      this.words.push(word);
+      return this.words;
+    } else if (
+      word[0] != this.words[this.words.length - 1].at(-1) ||
+      this.words.includes(word)
     ) {
       this.game_over = true;
-      return console.log("Game is Over!");
+      return "Game is Over!";
     }
     this.words.push(word);
     return this.words;
   };
   restart() {
-    this.game_over = true;
+    this.game_over = false;
     this.words = [];
+    return "game restarted";
   }
 }
 const game = new Shiritori();
 console.log(game.play("new"));
 console.log(game.play("war"));
+console.log(game.play("ram"));
+console.log(game.play("minus"));
+console.log(game.play("siren"));
+console.log(game.play("near"));
+console.log(game.play("ram"));
+console.log(game.restart());
+console.log(game.play("ram"));
 console.log(game.play("monkey"));
+console.log(game.play("y"));
+console.log(game.play("m"));
--- 08Classes/03Account.js
+++ 08Classes/03Account.js
@@ -3,14 +3,14 @@ class Account {
   constructor(name, balance) {
     this.name = name;
     this.#balance = balance;
-    id = Date.now() + Math.floor(Math.random() * 100);
+    const id = Date.now() + Math.floor(Math.random() * 100);
   }
   get balance() {
     return this.#balance;
   }
   set balance(newBalance) {
     if (newBalance < 0) {
-      throw new Error("Balance cannot be negative");
+      throw new Error("Balance cannot be negative!");
     }
     this.#balance = newBalance;
   }
@@ -23,8 +23,12 @@ class Account {
   debit(amount) {
     if (amount <= 0) {
       throw new Error("Wrong amount");
-    } else if ((this.#balance -= amount < 0)) {
-      throw new Error("Insufficient amount!");
+    } else if (this.#balance - amount < 0) {
+      throw new Error(
+        `Insufficient funds! \n${this.name} balance is: ${
+          this.#balance
+        }\nYou tried to transfer ${amount}`
+      );
     } else {
       this.#balance -= amount;
     }
@@ -42,13 +46,22 @@ saving.credit(5000);
 saving.debit(1000);
 saving.debit(2000);
 saving.transferTo(current, 1000);
-console.log(saving.getBalance());
-/*
 console.log(saving.balance);
 console.log(current.balance);
+/*
+> The rest does not make sense to me:
+
+identifyAccounts, this should be a static method for identify accounts by id of them
+
+> There was no id so far. Do we mean "name"?
+> What do we mean by identify?
+
 const res = Account.identifyAccounts(current, saving);
 console.log(saving.balance);
-saving.balance = 10000;
-saving.setBalance(20000);
+saving.balance = "hello";
+
+> Are we trying to set a string to balance? What is the meaning of setting balance to "hello"?
+
+saving.submitBalance("hello");
 console.log(saving);
 */
