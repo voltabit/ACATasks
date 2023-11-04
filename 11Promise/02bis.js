03cdb3907cdfe707c2151be8c21d3381d64e9291
Karo Kendimian <karo.kendimian@outlook.com>
2023-11-05 00:26:31 +0400

    HTML

---
+++ 11Promise/02bis.js
@@ -0,0 +1,11 @@
+function squarePromise(promise) {
+  try {
+  } catch (e) {}
+}
+
+const prom1 = new Promise((resolve, reject) => {
+  resolve(5); //number or string
+});
+const prom2 = new Promise((resolve, reject) => {
+  resolve("12");
+});
