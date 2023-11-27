eb5cf79881a101431c144ae4dc217efa83c9d09c
Karo Kendimian <chesmorana@gmail.com>
2023-11-27 17:56:17 +0400

    move fish

--- aquarium/aquarium.js
+++ aquarium/aquarium.js
@@ -1,52 +1,39 @@
 const aquarium = document.getElementById("aquarium");
 const fish = document.getElementById("fish");
+let fishAttributes = "";
+let flipped = false;
+let destination = { x: 0, y: 0 };
 
-let speed = 1;
-let direction = {
-  x: Math.random(),
-  y: Math.random() / 5,
-};
-let intervalId;
+function getCurrentPosition() {
+  return {
+    x: window.getComputedStyle(fish).getPropertyValue("padding-left"),
+    y: window.getComputedStyle(fish).getPropertyValue("padding-top"),
+  };
+}
 
-function moveFish() {
-  let fishRect = fish.getBoundingClientRect();
-  let aquariumRect = aquarium.getBoundingClientRect();
-
-  if (
-    fishRect.left <= aquariumRect.left ||
-    fishRect.right >= aquariumRect.right
-  ) {
-    direction.x *= -1;
-    changeDirection();
+function setDestination() {
+  const currentPosition = getCurrentPosition();
+  destination = {
+    x: Math.round(Math.random() * (aquarium.offsetWidth - 313)),
+    y: Math.round(Math.random() * (aquarium.offsetHeight - 174)),
+  };
+  const xPos = String(currentPosition.x).substring(0, -2);
+  console.log(xPos);
+  if (destination.x < xPos && !flipped) {
+    console.log("targetCoordinates " + destination.x + " " + xPos);
+    //flip fish
+    fishAttributes = `transition: all 5s ease-out allow-discrete; padding-left: ${destination.x}px; padding-top: ${destination.y}px; -moz-transform: scale(-1, 1); -o-transform: scale(-1, 1); -webkit-transform: scale(-1, 1); transform: scale(-1, 1);`;
+    fish.setAttribute("style", fishAttributes);
+    flipped = true;
+  } else if (flipped && destination.x > currentPosition.x) {
+    fishAttributes = `transition: all 5s ease-out allow-discrete; padding-left: ${destination.x}px; padding-top: ${destination.y}px`;
+    flipped = false;
+  } else {
+    fishAttributes = `transition: all 5s ease-out allow-discrete; padding-left: ${destination.x}px; padding-top: ${destination.y}px`;
   }
-  if (
-    fishRect.top <= aquariumRect.top ||
-    fishRect.bottom >= aquariumRect.bottom
-  ) {
-    direction.y *= -1;
-    changeDirection();
-  }
 
-  fish.style.left = Math.round((fish.offsetLeft + direction.x) * speed) + "px";
-  fish.style.top = Math.round((fish.offsetTop + direction.y) * speed) + "px";
+  fish.setAttribute("style", fishAttributes);
+  return destination;
 }
-
-function changeDirection() {
-  direction.x = Math.random() < 0.5 ? -1 : 1;
-  direction.y = Math.random() < 0.5 ? -1 : 1;
-}
-
-function speedUp() {
-  clearInterval(intervalId);
-  speed *= 4;
-  intervalId = setInterval(moveFish, 1000 / 60);
-  setTimeout(function () {
-    clearInterval(intervalId);
-    speed /= 4;
-    intervalId = setInterval(moveFish, 1000 / 60);
-  }, 1000);
-}
-
-fish.addEventListener("click", speedUp);
-intervalId = setInterval(moveFish, 1000 / 30);
-setInterval(changeDirection, 500);
+setDestination();
+setInterval(setDestination, 3000);
